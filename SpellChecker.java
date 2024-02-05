80993747c28c73074a708a2464bc419a203f2fb5
danielbeja23 <daniel.beja@post.runi.ac.il>
2024-02-06 00:21:31 +0200

    first

--- SpellChecker.java
+++ SpellChecker.java
@@ -8,19 +8,16 @@ public class SpellChecker {
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
-
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
-		return null;
+		str = str.substring(1, str.length());
+		return str;
 	}
 
 	public static int levenshtein(String word1, String word2) {
 		word1 = word1.toLowerCase();
 		word2 = word2.toLowerCase();
-		String tail1 = word1.substring(1);
-		String tail2 = word2.substring(1);
 		if (word1.length() == 0) {
 			return word2.length();
 		}
@@ -29,10 +26,10 @@ public class SpellChecker {
 		}
 
 		if (word1.charAt(0) == word2.charAt(0)) {
-			levenshtein(tail1, tail2);
+			levenshtein(tail(word1), tail(word2));
 		} else {
-			return 1 + (Math.min(Math.min(levenshtein(tail1, word2), levenshtein(word1, tail2)),
-					levenshtein(tail1, tail2)));
+			return 1 + (Math.min(Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2))),
+					levenshtein(tail(word1), tail(word2))));
 		}
 		return 0;
 
@@ -40,12 +37,12 @@ public class SpellChecker {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 		return dictionary;
+
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
