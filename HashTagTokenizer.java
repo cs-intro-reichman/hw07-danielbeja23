bf8e0e87b5cad52879a44a5c60ece98fa7b0d7af
danielbeja23 <daniel.beja@post.runi.ac.il>
2024-02-05 20:44:31 +0200

    first

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -1,40 +1,38 @@
 
-
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
 
 		String hashTag = args[0];
-		String []dictionary = readDictionary("dictionary.txt");
+		String[] dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 		return dictionary;
 	}
 
-	public static boolean existInDictionary(String word, String []dictionary) {
+	public static boolean existInDictionary(String word, String[] dictionary) {
 		// Your code here
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
 
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
-            return;
-        }
- 
-        int N = hashtag.length();
+		if (hashtag.isEmpty()) {
+			return;
+		}
 
-        for (int i = 1; i <= N; i++) {
-		
-        }
-    }
+		int N = hashtag.length();
+
+		for (int i = 1; i <= N; i++) {
+
+		}
+	}
 
 }
